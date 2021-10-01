fce7b862910ca72c1fdb8a6d7f67781e3aa27990
Shreyas <sunku.shreyas8@gmail.com>
2021-10-01 21:07:28 +0530

    Updated

--- Project_Movie/settings.py
+++ Project_Movie/settings.py
@@ -130,12 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -144,8 +143,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-# Configure Django App for Heroku.
-import django_heroku
-
-django_heroku.settings(locals())
